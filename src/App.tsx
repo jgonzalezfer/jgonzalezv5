3c54ed84bb02843a1217ffd7e8b619cea2c2c43d
Joel Gonzalez Fernandez <joel.gonzalez5115@gmail.com>
2025-03-28 11:32:01 -0300

    Eliminación de la sección de proyectos en App.tsx: se retira el componente ProjectSection y su contenido, optimizando así la estructura del archivo y mejorando la claridad del código.

--- src/App.tsx
+++ src/App.tsx
@@ -27,18 +27,7 @@ function App() {
           <Route path="/sobremiCvJoelPage" element={<SobremiCvJoelPage />} />
         </Routes>
       </Router>
-      <div className="min-h-screen bg-black p-4">
-        <ProjectSection
-          title="Gucci G-Flat: Lujo minimalista al alcance de todos"
-          description="La colección Gucci G-Flat emerge como un testimonio del arte del diseño contemporáneo, combinando innovación con una rica herencia."
-          image="https://example.com/path-to-gucci-image.jpg"
-          timestamp="hace 2 horas"
-          source={{
-            name: "AmericaMalls & Retail",
-            icon: "https://example.com/path-to-icon.jpg"
-          }}
-        />
-      </div>
+
     </ThemeProvider>
   );
 }
