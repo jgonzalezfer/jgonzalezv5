03ffdbcd6eb187af75558e5412309127b9b312e9
Joel Gonzalez Fernandez <joel.gonzalez5115@gmail.com>
2025-03-30 18:27:09 -0300

    Se actualiza la configuración de Vite: se incluye soporte para archivos Markdown en la propiedad assetsInclude y se añaden nuevas extensiones en la resolución de módulos, mejorando la flexibilidad del proyecto al permitir la importación de diferentes tipos de archivos.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.md'],
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.md']
+  }
 })
